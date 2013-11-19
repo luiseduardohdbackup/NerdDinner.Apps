0991aadbc79cc30c822827bec5b7eb1b0318f6b7
pmourfield <pete@mourfield.com>
2013-11-19 18:28:39 -0500

    added rsvps

--- NerdDinner.Core/Dinner.cs
+++ NerdDinner.Core/Dinner.cs
@@ -1,14 +1,20 @@
 ﻿
 using System;
+using System.Collections.Generic;
 namespace NerdDinner.Core
 {
     // this class is partial so we can do MetadataType validations in a higher layer
     // if the platform supports it
     public partial class Dinner
     {
+        public Dinner()
+        {
+            RSVPs = new List<RSVP>();
+        }
         public string Title { get; set; }
         public DateTime EventDate { get; set; }
         public string Description { get; set; }
         public string HostedBy { get; set; }
+        public virtual ICollection<RSVP> RSVPs { get; set; }
     }
 }
---
+++ NerdDinner.Core/RSVP.cs
@@ -0,0 +1,11 @@
+﻿﻿
+namespace NerdDinner.Core
+{
+    public class RSVP
+    {
+        public int RsvpID { get; set; }
+        public int DinnerID { get; set; }
+        public string AttendeeName { get; set; }
+        public virtual Dinner Dinner { get; set; }
+    }
+}
