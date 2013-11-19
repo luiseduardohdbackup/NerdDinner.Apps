45ae8930ecc0228134c54009ccaa9e063dd69bf0
pmourfield <pete@mourfield.com>
2013-11-19 18:00:03 -0500

    small change

--- NerdDinner.Core/Dinner.cs
+++ NerdDinner.Core/Dinner.cs
@@ -2,7 +2,9 @@
 using System;
 namespace NerdDinner.Core
 {
-    public class Dinner
+    // this class is partial so we can do MetadataType validations in a higher layer
+    // if the platform supports it
+    public partial class Dinner
     {
         public string Title { get; set; }
         public DateTime EventDate { get; set; }
